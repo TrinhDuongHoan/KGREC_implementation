ae7294ef0febddab96a51ee2f89c0d23cb0a7f59
TrinhDuongHoan <trinhduonghoan2004@gmail.com>
2025-11-14 02:23:53 +0700

    no seed kgrec

--- training/train_kgrec.py
+++ training/train_kgrec.py
@@ -76,10 +76,10 @@ def evaluate(model, dataloader, Ks, device):
 
 def train(args):
     # seed
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed_all(args.seed)
+    # random.seed(args.seed)
+    # np.random.seed(args.seed)
+    # torch.manual_seed(args.seed)
+    # torch.cuda.manual_seed_all(args.seed)
 
     log_save_id = create_log_id(args.save_dir)
     logging_config(folder=args.save_dir, name='log{:d}'.format(log_save_id), no_console=False)
